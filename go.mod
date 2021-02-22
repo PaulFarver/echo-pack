8ff9e419fff4568ccdc1dc46b99179247b9a8038
Paul Farver <paulfarver@gmail.com>
2021-02-22 23:55:55 +0100

    Implement simple stats

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/paulfarver/echo-pack
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.2.0
+require (
+	github.com/labstack/echo/v4 v4.2.0
+	github.com/prometheus/client_golang v1.9.0
+)
--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -1,18 +1,70 @@
 package prometheus
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Prometheus struct {
+	r              *prometheus.Registry
+	Skipper        middleware.Skipper
+	requestCounter *prometheus.CounterVec
+	requestSummary *prometheus.SummaryVec
+	pathLabeler    func(c echo.Context) string
+}
+
+func DefaultPathLabeler(c echo.Context) string {
+	return c.Path()
 }
 
 func NewPrometheus() *Prometheus {
-	return nil
+	r := prometheus.NewRegistry()
+	requestCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Subsystem: "",
+		Name:      "",
+		Help:      "",
+	}, []string{"status", "path"})
+	requestSummary := prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Subsystem:  "",
+		Name:       "",
+		Help:       "",
+		Objectives: map[float64]float64{},
+	}, []string{"status", "path"})
+	r.Register(requestCounter)
+	return &Prometheus{
+		r:              r,
+		Skipper:        middleware.DefaultSkipper,
+		requestCounter: requestCounter,
+		requestSummary: requestSummary,
+	}
 }
 
 func (p *Prometheus) Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return next(c)
+		if p.Skipper(c) {
+			return next(c)
+		}
+
+		start := time.Now()
+
+		res := next(c)
+
+		status := c.Response().Status
+		elapsed := time.Since(start)
+
+		p.requestCounter.WithLabelValues(fmt.Sprint(status), p.pathLabeler(c)).Inc()
+		p.requestSummary.WithLabelValues(fmt.Sprint(status), p.pathLabeler(c)).Observe(elapsed.Seconds())
+
+		return res
 	}
+}
+
+func (p *Prometheus) Expose() echo.HandlerFunc {
+	h := promhttp.HandlerFor(p.r, promhttp.HandlerOpts{})
+	return echo.WrapHandler(h)
 }
