77728d6bcf66ce9b111dc2c12d55fccd1ddbb44a
Paul Farver <paulfarver@gmail.com>
2021-06-15 14:58:39 +0200

    Add requestid middleware
    Signed-off-by: Paul Farver <paulfarver@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/labstack/echo/v4 v4.2.0
+	github.com/labstack/gommon v0.3.0
 	github.com/prometheus/client_golang v1.9.0
 	github.com/sirupsen/logrus v1.6.0
 )
---
+++ requestid/requestid.go
@@ -0,0 +1,73 @@
+package requestid
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/random"
+)
+
+type (
+	// RequestIDConfig defines the config for RequestID middleware.
+	RequestIDConfig struct {
+		// Header defines the name of the header to use
+		Header string
+
+		// Skipper defines a function to skip middleware.
+		Skipper middleware.Skipper
+
+		// Generator defines a function to generate an ID.
+		// Optional. Default value random.String(32).
+		Generator func() string
+	}
+)
+
+var (
+	// DefaultRequestIDConfig is the default RequestID middleware config.
+	DefaultRequestIDConfig = RequestIDConfig{
+		Header:    echo.HeaderXRequestID,
+		Skipper:   middleware.DefaultSkipper,
+		Generator: generator,
+	}
+)
+
+// RequestID returns a X-Request-ID middleware.
+func RequestID() echo.MiddlewareFunc {
+	return RequestIDWithConfig(DefaultRequestIDConfig)
+}
+
+// RequestIDWithConfig returns a X-Request-ID middleware with config.
+func RequestIDWithConfig(config RequestIDConfig) echo.MiddlewareFunc {
+	// Defaults
+	if config.Header == "" {
+		config.Header = DefaultRequestIDConfig.Header
+	}
+
+	if config.Skipper == nil {
+		config.Skipper = DefaultRequestIDConfig.Skipper
+	}
+	if config.Generator == nil {
+		config.Generator = generator
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if config.Skipper(c) {
+				return next(c)
+			}
+
+			req := c.Request()
+			res := c.Response()
+			rid := req.Header.Get(config.Header)
+			if rid == "" {
+				rid = config.Generator()
+			}
+			res.Header().Set(config.Header, rid)
+
+			return next(c)
+		}
+	}
+}
+
+func generator() string {
+	return random.String(32)
+}
